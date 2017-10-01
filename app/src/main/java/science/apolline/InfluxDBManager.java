4b4de3474ef40f68f2a8c284466390fcc774a89f
Aoudia <mf.aoudia@gmail.com>
2017-10-02 01:19:20 +0200

    perf(influxDBManager) : Add BachPoints support
    Add ability to push to multi databases and measurements.
    Change writePoint() JSON argument from JSONObject (android) to String.
    Remove attributs : Gas, Humidity, Temeerature and replace it by DATA simplify source code.
    Add GSON library dependency to Gradle.

--- app/src/main/java/science/apolline/InfluxDBManager.java
+++ app/src/main/java/science/apolline/InfluxDBManager.java
@@ -1,11 +1,14 @@
 package science.apolline;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.BatchPoints;
 import org.influxdb.dto.Point;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -17,48 +20,66 @@ public class InfluxDBManager {
 
     public final String DEVICE = "deviceId";
     public final String SENSOR = "sensorId";
-    public final String GAS = "gasId";
-    public final String TEMPERATURE = "temperatureId";
-    public final String HUMIDITY = "humidityId";
-    public final String PM = "pmId";
+    public final String DATA = "gasId";
     public final String UNIT = "unitId";
     public final String VALUE = "value";
-    public final String UNKNOW = "unknow";
 
 
     public InfluxDB configInfluxDB(String dbName, String dbUrlPort, String dbUser, String dbPassword) {
 
         InfluxDB influxDB = InfluxDBFactory.connect(dbUrlPort, dbUser, dbPassword);
-        influxDB.setLogLevel(InfluxDB.LogLevel.FULL);
-        influxDB.setConsistency(InfluxDB.ConsistencyLevel.ALL);
+        influxDB.setLogLevel(InfluxDB.LogLevel.NONE);
+        influxDB.setConsistency(InfluxDB.ConsistencyLevel.QUORUM);
         influxDB.createDatabase(dbName);
+        // Flush every 2000 Points, at least every 100ms
+        influxDB.enableBatch(2000, 100, TimeUnit.NANOSECONDS);
 
         return influxDB;
     }
 
 
-    public void writePoint(InfluxDB influxDB, String dbMeasurment, JSONObject json) throws JSONException {
+    public void writePoint(InfluxDB influxDB, String dbName, String dbMeasurment, String jsonArg) {
+
+        BatchPoints batchPoints = BatchPoints
+                .database(dbName)
+                .build();
+
+        JsonParser parser = new JsonParser();
+        JsonElement jsonElement = parser.parse(jsonArg);
+
 
         Point.Builder pointBuilder = Point.measurement(dbMeasurment)
                 .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS);
 
-        pointBuilder.addField(DEVICE, json.optString(DEVICE, UNKNOW))
-                .addField(SENSOR, json.optString(SENSOR, UNKNOW));
+        if (jsonElement.isJsonObject()) {
 
-        if (json.optString(GAS) != null) {
-            pointBuilder.addField(GAS, json.optString(GAS, UNKNOW));
-        } else if (json.optString(TEMPERATURE) != null) {
-            pointBuilder.addField(TEMPERATURE, json.optString(TEMPERATURE, UNKNOW));
-        } else if (json.optString(HUMIDITY) != null) {
-            pointBuilder.addField(HUMIDITY, json.optString(HUMIDITY, UNKNOW));
-        } else if (json.optString(PM) != null) {
-            pointBuilder.addField(PM, json.optString(PM, UNKNOW));
+            JsonObject json = jsonElement.getAsJsonObject();
+
+            pointBuilder.tag(DEVICE, json.get(DEVICE).getAsString())
+                    .tag(SENSOR, json.get(SENSOR).getAsString());
+
+            pointBuilder.tag(UNIT, json.get(UNIT).getAsString());
+
+
+            if (json.has(DATA)) {
+                JsonObject tmp = json.get(DATA).getAsJsonObject();
+
+                Set<Map.Entry<String, JsonElement>> entries = tmp.entrySet();
+                for (Map.Entry<String, JsonElement> entry : entries) {
+
+                    pointBuilder.tag(DATA, entry.getKey());
+                    pointBuilder.addField(VALUE, entry.getValue().toString());
+
+                    Point point = pointBuilder.build();
+                    batchPoints.point(point);
+                    influxDB.write(batchPoints);
+
+                }
+
+            }
+
         }
 
-        pointBuilder.addField(UNIT, json.optString(UNIT, UNKNOW))
-                .addField(VALUE, json.optInt(VALUE, -1));
-
-        influxDB.write(pointBuilder.build());
 
     }
 
