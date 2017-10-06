63cfded816625cfa590b6517f38deb17fae7a6c8
Aoudia <moncef.aoudia@inria.fr>
2017-10-06 14:57:37 +0200

    perf(InfluxDBManager) : Improve InfluxDB write methode.
    Comment create database option to push to Apolline server (privilege problems).
    Improve write function.

--- app/src/main/java/science/apolline/InfluxDBManager.java
+++ app/src/main/java/science/apolline/InfluxDBManager.java
@@ -3,10 +3,12 @@ package science.apolline;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.BatchPoints;
 import org.influxdb.dto.Point;
+
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -18,19 +20,20 @@ import java.util.concurrent.TimeUnit;
 public class InfluxDBManager {
 
 
-    public final String DEVICE = "deviceId";
-    public final String SENSOR = "sensorId";
-    public final String DATA = "gasId";
-    public final String UNIT = "unitId";
+    public final String DEVICE = "device";
+    public final String SENSOR = "sensor";
+    public final String DATA = "gas";
+    public final String UNIT = "unit";
     public final String VALUE = "value";
+    public final String DATE = "date";
 
 
     public InfluxDB configInfluxDB(String dbName, String dbUrlPort, String dbUser, String dbPassword) {
 
         InfluxDB influxDB = InfluxDBFactory.connect(dbUrlPort, dbUser, dbPassword);
-        influxDB.setLogLevel(InfluxDB.LogLevel.NONE);
+        influxDB.setLogLevel(InfluxDB.LogLevel.FULL);
         influxDB.setConsistency(InfluxDB.ConsistencyLevel.QUORUM);
-        influxDB.createDatabase(dbName);
+        // influxDB.createDatabase(dbName);
         // Flush every 2000 Points, at least every 100ms
         influxDB.enableBatch(2000, 100, TimeUnit.NANOSECONDS);
 
@@ -38,7 +41,7 @@ public class InfluxDBManager {
     }
 
 
-    public void writePoint(InfluxDB influxDB, String dbName, String dbMeasurment, String jsonArg) {
+    public void writePoint(InfluxDB influxDB, String dbName, String jsonArg) {
 
         BatchPoints batchPoints = BatchPoints
                 .database(dbName)
@@ -47,19 +50,9 @@ public class InfluxDBManager {
         JsonParser parser = new JsonParser();
         JsonElement jsonElement = parser.parse(jsonArg);
 
-
-        Point.Builder pointBuilder = Point.measurement(dbMeasurment)
-                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS);
-
         if (jsonElement.isJsonObject()) {
 
             JsonObject json = jsonElement.getAsJsonObject();
-
-            pointBuilder.tag(DEVICE, json.get(DEVICE).getAsString())
-                    .tag(SENSOR, json.get(SENSOR).getAsString());
-
-            pointBuilder.tag(UNIT, json.get(UNIT).getAsString());
-
 
             if (json.has(DATA)) {
                 JsonObject tmp = json.get(DATA).getAsJsonObject();
@@ -67,16 +60,22 @@ public class InfluxDBManager {
                 Set<Map.Entry<String, JsonElement>> entries = tmp.entrySet();
                 for (Map.Entry<String, JsonElement> entry : entries) {
 
-                    pointBuilder.tag(DATA, entry.getKey());
-                    pointBuilder.addField(VALUE, entry.getValue().toString());
+                    Point.Builder pointBuilder = Point.measurement(entry.getKey())
+                            .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                            .addField(VALUE, entry.getValue().toString())
+                            .addField(DATE, json.get(DATE).getAsString())
+                            .tag(DEVICE, json.get(DEVICE).getAsString())
+                            .tag(SENSOR, json.get(SENSOR).getAsString())
+                            .tag(UNIT, json.get(UNIT).getAsString());
 
                     Point point = pointBuilder.build();
                     batchPoints.point(point);
-                    influxDB.write(batchPoints);
 
                 }
 
             }
+
+            influxDB.write(batchPoints);
 
         }
 
