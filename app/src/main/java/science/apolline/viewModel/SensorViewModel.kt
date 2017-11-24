0552473292cfe264dd4cc77c9f76005070b79e7f
kaal-dam <dam.toulouse@gmail.com>
2017-11-24 14:36:59 +0100

    Style : change package archi to be closer of MVVM one

--- app/src/main/java/science/apolline/viewModel/SensorViewModel.kt
+++ app/src/main/java/science/apolline/viewModel/SensorViewModel.kt
@@ -1,5 +1,6 @@
-package science.apolline.sensor.common
+package science.apolline.viewModel
 
+import android.Manifest
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
@@ -7,68 +8,66 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.support.v4.content.ContextCompat
 import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
 import android.widget.Toast
 import retrofit2.Call
 import retrofit2.Response
 import science.apolline.R
-import science.apolline.database.AppDatabase
-import science.apolline.database.SensorDao
-import science.apolline.models.Post
-import science.apolline.models.Sensor
-import science.apolline.networks.ApiService
-import science.apolline.networks.ApiUtils
+import science.apolline.service.database.AppDatabase
+import science.apolline.service.database.SensorDao
+import science.apolline.models.InfluxBody
+import science.apolline.models.Device
+import science.apolline.service.networks.ApiService
+import science.apolline.service.networks.ApiUtils
+import science.apolline.models.IntfSensorData
 import science.apolline.utils.RequestParser
 import java.io.IOException
 import java.util.*
 
-class sensorViewModel (application: Application) : AndroidViewModel(application){
+class SensorViewModel(application: Application) : AndroidViewModel(application){
     init {
         val BReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context, intent: Intent) {
                 Log.e("viewModel","receiver")
-                val data : sensorData = intent.getParcelableExtra("dataBundle")
+                val data : IntfSensorData = intent.getParcelableExtra("dataBundle")
                 dataLive.postValue(data)
 
                 Log.e("viewModel","${dataLive.hasActiveObservers()}");
-                //TODO : move that to sensor sendBroadcast as intent extra
+                //TODO : move that to Device sendBroadcast as intent extra
                 val calendar = Calendar.getInstance()
                 var d1 = calendar.time
                 //TODO position
-                //TODO device and sensor param as intent extra
-                val sensor : Sensor = Sensor("IOIO","IOIO",d1.toString(),null,dataLive.value?.toJson())
-                setPersistant(sensor)
-                //sendData(sensor)
+
+                //TODO device and Device param as intent extra
+                val device: Device = Device("IOIO",d1.toString(),null,dataLive.value?.toJson())
+                setPersistant(device)
+                //sendData(Device)
             }
         }
         LocalBroadcastManager.getInstance(application).registerReceiver(BReceiver, IntentFilter(application.getString(R.string.dataBroadcastFilter)))
     }
 
-    var dataLive : MutableLiveData<sensorData> = MutableLiveData()
+    var dataLive : MutableLiveData<IntfSensorData> = MutableLiveData()
 
-    private fun sendData(sensor: Sensor) {
-        val requestBody: String = RequestParser.createRequestBody(sensor)
+    private fun sendData(device: Device) {
+        val requestBody: String = RequestParser.createRequestBody(device)
         val api : ApiService = ApiUtils.apiService
-        val postCall : Call<Post> = api.savePost("test","toto","root",requestBody)
-        val postResponse: Response<Post>
-        try {
-            postResponse = postCall.execute();
-            if (postResponse.isSuccessful()){
-                Toast.makeText(getApplication(),"Data send: success",Toast.LENGTH_SHORT).show();
-            }
-            else{
-                Toast.makeText(getApplication(),"Data send: Failure, message = "+postResponse.message(),Toast.LENGTH_SHORT).show();
-            }
-        } catch (e:IOException) {
-            Toast.makeText(getApplication(),"Unable to send data",Toast.LENGTH_SHORT).show();
+        val postCall : Call<InfluxBody> = api.savePost("test","toto","root",requestBody)
+        val postResponse: Response<InfluxBody>
+        postResponse = postCall.execute();
+        if (postResponse.isSuccessful()) {
+            Toast.makeText(getApplication(), "Data send: success", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplication(), "Data send: Failure, message = " + postResponse.message(), Toast.LENGTH_SHORT).show();
         }
     }
 
-    private fun setPersistant(sensor: Sensor) {
+    private fun setPersistant(device: Device) {
 
         val sensorModel: SensorDao = AppDatabase.getInstance(getApplication())
-        sensorModel.insertOne(sensor)
+        sensorModel.insertOne(device)
     }
 //    BReceiver = new BroadcastReceiver() {
 //        @Override
@@ -97,15 +96,15 @@ class sensorViewModel (application: Application) : AndroidViewModel(application)
 //
 //
 ////TODO localisation
-//            Sensor sensor = new Sensor("IOIO","IOIO",d1.toString(),null,data.toJson());
+//            Device sensor = new Device("IOIO","IOIO",d1.toString(),null,data.toJson());
 //
 //            AppDatabase appDatabase = AppDatabase.Companion.getAppDatabase(getActivity());
 //            appDatabase.SensorModel().insertOne(sensor);
 //
 //            String requestBody = RequestParser.INSTANCE.createRequestBody(sensor);
 //            ApiService api = ApiUtils.INSTANCE.getApiService();
-//            Call<Post> postCall = api.savePost("test", "toto", "root", requestBody);
-//            Response<Post> postResponse;
+//            Call<InfluxBody> postCall = api.savePost("test", "toto", "root", requestBody);
+//            Response<InfluxBody> postResponse;
 //            try {
 //                postResponse= postCall.execute();
 //                if (postResponse.isSuccessful()){
