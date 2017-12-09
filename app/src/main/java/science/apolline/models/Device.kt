01ffbb4348dbe9ce8b7d550fe98ab76400e8d61b
Aoudia <mf.aoudia@gmail.com>
2017-12-09 02:36:44 +0100

    refactor(*) : Add CSV export
    Device.kt: add toHeader() to create first line of CSV file.
    add toArray() to transforme current object to array.
    Build.gradle: downgrade SDK from 27 to 26 (more stable).
    TravisCi: fix TravisCI.
    close #14

--- app/src/main/java/science/apolline/models/Device.kt
+++ app/src/main/java/science/apolline/models/Device.kt
@@ -40,4 +40,44 @@ data class Device(
         """.trimMargin()
     }
 
+
+    fun toHeader(): Array<String> {
+        val headerArray = mutableListOf<String>()
+        headerArray.add("SensorID")
+        headerArray.add("Device")
+        headerArray.add( "Date")
+        headerArray.add( "Latitude")
+        headerArray.add( "Longitude")
+        headerArray.add( "Provider")
+        headerArray.add( "Transport")
+        val temp = data!!.entrySet().iterator()
+        while (temp.hasNext()) {
+            val it = temp.next()
+            headerArray.add(it.key)
+        }
+        return headerArray.toTypedArray()
+    }
+
+    fun toArray(): Array<String> {
+        val objectArray = mutableListOf<String>()
+        objectArray.add(sensorId.toString())
+        objectArray.add(device)
+        objectArray.add(date)
+        objectArray.add(position?.latitude.toString())
+        objectArray.add( position?.longitude.toString())
+        objectArray.add(position?.provider.orEmpty())
+        objectArray.add(position?.transport.orEmpty())
+        val temp = data!!.entrySet().iterator()
+        while (temp.hasNext()) {
+            val it = temp.next()
+            val value = it.value.asJsonArray[0].toString()
+            objectArray.add(value)
+        }
+        return objectArray.toTypedArray()
+    }
+
+
+
+
+
 }
