7ac4ceb5db52c1f6d88addfc83bdaa15135398b1
Aoudia <mf.aoudia@gmail.com>
2018-01-19 14:46:59 +0100

    refactor : Fix unit and instrumented tests

--- app/src/androidTest/java/science/apolline/RoomDatabaseTest.kt
+++ app/src/androidTest/java/science/apolline/RoomDatabaseTest.kt
@@ -22,6 +22,7 @@ import java.util.concurrent.TimeUnit
  */
 
 
+@Suppress("UNCHECKED_CAST")
 @RunWith(AndroidJUnit4::class)
 class RoomDatabaseTest {
 
@@ -35,7 +36,7 @@ class RoomDatabaseTest {
 
 
     @Test
-    fun InsertSensorTest() {
+    fun insertSensorTest() {
 
         //given
         val dataList = "{" +
@@ -48,15 +49,15 @@ class RoomDatabaseTest {
         val gson = Gson()
         val dataListObject = gson.fromJson(dataList, JsonObject::class.java)
         val positionInitObject = Position("GPS", 152.36, 142.36, "Train")
-        val sensor =Device("ffffffff-c9cf-31db-0000-00006c125b14","Arduino",  1422568543702900257, positionInitObject, dataListObject ,0)
+        val sensor = Device("ffffffff-c9cf-31db-0000-00006c125b14","Arduino",  1422568543702900257, positionInitObject, dataListObject ,0)
 
         sensorDao?.insertOne(sensor)
-        val sensorTest = getValue(sensorDao?.getSensorById(sensor.id)!!)
+        val sensorTest = getValue(sensorDao?.getSensorById(1)!!)
         Assert.assertEquals(sensor.device, sensorTest.device)
     }
 
     @Test
-    fun FlushAllSensorDataTest(){
+    fun flushAllSensorDataTest(){
         sensorDao?.flushSensorData()
         Assert.assertEquals(sensorDao?.getSensorCount(), 0)
     }
