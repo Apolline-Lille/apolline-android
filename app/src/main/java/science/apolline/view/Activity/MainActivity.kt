31e8113fc7d391d09bed843a31b1c4f81cd23f7e
Aoudia <mf.aoudia@gmail.com>
2018-01-19 16:07:24 +0100

    refactor(Sync) : Add sync button.
    Add sync button to appbar.
    Fix Fragment context for IOIOFragment.

--- app/src/main/java/science/apolline/view/Activity/MainActivity.kt
+++ app/src/main/java/science/apolline/view/Activity/MainActivity.kt
@@ -87,6 +87,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
+
+
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
