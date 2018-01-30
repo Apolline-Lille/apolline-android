46f1e73a057be5a9c1002f1992622178ca16b357
Aoudia <mf.aoudia@gmail.com>
2018-01-30 16:20:47 +0100

    refactor(database) : Add file provider
    Add file provider to manage URI for API>= 23.

--- app/src/main/java/science/apolline/utils/DataExport.kt
+++ app/src/main/java/science/apolline/utils/DataExport.kt
@@ -3,9 +3,6 @@ package science.apolline.utils
 import android.content.Context
 import android.os.Environment.getExternalStorageDirectory
 import android.util.Log
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.toast
-import org.jetbrains.anko.uiThread
 import science.apolline.service.database.AppDatabase
 import java.io.File
 import java.io.FileWriter
@@ -13,15 +10,18 @@ import com.google.gson.GsonBuilder
 import com.opencsv.CSVWriter
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
+import android.support.v4.content.FileProvider
 import com.google.gson.JsonObject
 import science.apolline.R
+import org.jetbrains.anko.*
 
 
 /**
  * Created by damien on 08/12/2017.
  */
 
-class DataExport {
+class DataExport : AnkoLogger {
 
     fun toHeader(data: JsonObject?): Array<String> {
         val headerArray = mutableListOf<String>()
@@ -68,8 +68,10 @@ class DataExport {
     fun exportToCsv(context: Context) {
 
         val folder = File(getExternalStorageDirectory().toString() + "/Apolline")
+
         if (!folder.exists())
             folder.mkdir()
+
         val filenameCSV = folder.toString() + "/" + "data.csv"
 
         doAsync {
@@ -83,11 +85,20 @@ class DataExport {
             dataList.forEach {
                 entries.add(it.toArray())
             }
+
             CSVWriter(FileWriter(filenameCSV)).use { writer -> writer.writeAll(entries) }
 
+
             uiThread {
-                val file = File(filenameCSV)
-                val uri = Uri.fromFile(file)
+
+                val file = File(folder,"data.csv")
+                val uri :Uri
+                uri = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+                    Uri.fromFile(file)
+                }else{
+                    FileProvider.getUriForFile(context, "science.apolline.fileprovider", file)
+                }
+
                 val shareIntent = Intent()
                 shareIntent.action = Intent.ACTION_SEND
                 shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
