ff114240323cc07762f9de7f6365330f1430dd7c
Romain Rouvoy <romain.rouvoy@gmail.com>
2018-03-07 11:22:43 +0100

    refactor(IOIO): Clean the code of the IOIO fragment.

--- app/src/main/java/science/apolline/view/fragment/IOIOFragment.kt
+++ app/src/main/java/science/apolline/view/fragment/IOIOFragment.kt
@@ -46,8 +46,7 @@ import science.apolline.viewModel.SensorViewModel
 
 class IOIOFragment : RootFragment(), OnChartValueSelectedListener, FragmentLifecycle, AnkoLogger {
 
-
-    private var mReferenceTimestamp: Long = MIN_TIME_STAMP
+    private var mReferenceTimestamp: Long = MIN_TIMESTAMP
 
     private lateinit var mDataList: List<ILineDataSet>
 
@@ -70,7 +69,6 @@ class IOIOFragment : RootFragment(), OnChartValueSelectedListener, FragmentLifec
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mDisposable = CompositeDisposable()
-        // mViewModel = SensorViewModel(activity!!.application)
 
         floating_action_menu_json.setOnClickListener {
             exportToJson(activity!!.application)
@@ -86,160 +84,108 @@ class IOIOFragment : RootFragment(), OnChartValueSelectedListener, FragmentLifec
         fragment_ioio_progress_pm2.setTextMode(TextMode.VALUE)
         fragment_ioio_progress_pm10.setTextMode(TextMode.VALUE)
 
-        createGraphMultiSets()
-        setupGraphView()
-
+        mDataList = createParticleGauges()
+        configureParticleHistory()
     }
 
 
-    private fun createGraphMultiSets() {
+    private fun createParticleGauges() : List<ILineDataSet> {
+        val setPM1 = createParticleGauge("PM1", Color.rgb(48, 79, 254))
+        val setPM2 = createParticleGauge("PM2.5", Color.rgb(0, 200, 83))
+        val setPM10 = createParticleGauge("PM10", Color.rgb(213, 0, 0))
 
-        val setPM1 = LineDataSet(null, "PM1")
-        setPM1.axisDependency = YAxis.AxisDependency.LEFT
-        setPM1.color = Color.rgb(48, 79, 254)
-        setPM1.setCircleColor(Color.rgb(48, 79, 254))
-        setPM1.lineWidth = 2f
-        setPM1.circleRadius = 4f
-        setPM1.fillAlpha = 65
-        setPM1.fillColor = Color.rgb(48, 79, 254)
-        setPM1.highLightColor = Color.rgb(244, 117, 117)
-        setPM1.valueTextColor = Color.rgb(48, 79, 254)
-        setPM1.valueTextSize = 9f
-        setPM1.setDrawValues(false)
+        return listOf(setPM1, setPM2, setPM10)
+    }
 
+    private fun createParticleGauge(label: String, color: Int): LineDataSet {
+        val gauge = LineDataSet(null, label)
 
-        val setPM2 = LineDataSet(null, "PM2.5")
-        setPM2.axisDependency = YAxis.AxisDependency.LEFT
-        setPM2.color = Color.rgb(0, 200, 83)
-        setPM2.setCircleColor(Color.rgb(0, 200, 83))
-        setPM2.lineWidth = 2f
-        setPM2.circleRadius = 4f
-        setPM2.fillAlpha = 65
-        setPM2.fillColor = Color.rgb(0, 200, 83)
-        setPM2.highLightColor = Color.rgb(244, 117, 117)
-        setPM2.valueTextColor = Color.rgb(0, 200, 83)
-        setPM2.valueTextSize = 9f
-        setPM2.setDrawValues(false)
+        gauge.axisDependency = YAxis.AxisDependency.LEFT
+        gauge.color = color
+        gauge.setCircleColor(color)
+        gauge.lineWidth = 2f
+        gauge.circleRadius = 4f
+        gauge.fillAlpha = 65
+        gauge.fillColor = color
+        gauge.highLightColor = Color.rgb(244, 117, 117)
+        gauge.valueTextColor = color
+        gauge.valueTextSize = 9f
+        gauge.setDrawValues(false)
 
-        val setPM10 = LineDataSet(null, "PM10")
-        setPM10.axisDependency = YAxis.AxisDependency.LEFT
-        setPM10.color = Color.rgb(213, 0, 0)
-        setPM10.setCircleColor(Color.rgb(213, 0, 0))
-        setPM10.lineWidth = 2f
-        setPM10.circleRadius = 4f
-        setPM10.fillAlpha = 65
-        setPM10.fillColor = Color.rgb(213, 0, 0)
-        setPM10.highLightColor = Color.rgb(244, 117, 117)
-        setPM10.valueTextColor = Color.rgb(213, 0, 0)
-        setPM10.valueTextSize = 9f
-        setPM10.setDrawValues(false)
-
-        mDataList = listOf(setPM1, setPM2, setPM10)
-
+        return gauge
     }
 
 
-    private fun setupGraphView() {
+    private fun configureParticleHistory() {
+        mReferenceTimestamp = now()
 
-        mReferenceTimestamp = System.currentTimeMillis() / 1000
+        chart.marker = CustomMarkerView(context!!, R.layout.graph_custom_marker, mReferenceTimestamp)
 
-        val marker = CustomMarkerView(context!!, R.layout.graph_custom_marker, mReferenceTimestamp)
-        chart.marker = marker
-
-        // LineTimeChart
         chart.setOnChartValueSelectedListener(this)
-        // enable description text
         chart.description.isEnabled = false
-
 
         chart.dragDecelerationFrictionCoef = 0.9f
         chart.isHighlightPerDragEnabled = true
 
-        // set an alternative background color
-        //        chart.setBackgroundColor(Color.WHITE);
-        //        chart.setViewPortOffsets(0f, 0f, 0f, 0f);
-
-        // enable touch gestures
         chart.setTouchEnabled(true)
-        // enable scaling and dragging
         chart.isDragEnabled = true
         chart.setScaleEnabled(true)
         chart.setDrawGridBackground(false)
-        // if disabled, scaling can be done on x- and y-axis separately
         chart.setPinchZoom(true)
-        // set an alternative background color
         chart.setBackgroundColor(Color.TRANSPARENT)
-
 
         val data = LineData()
         data.setValueTextColor(Color.WHITE)
-        // add empty data
         chart.data = data
-
         chart.invalidate()
 
-        // get the legend (only possible after setting data)
-        val l = chart.legend
-        // modify the legend ...
-        l.form = Legend.LegendForm.LINE
-        l.typeface = Typeface.DEFAULT
-        l.textColor = Color.BLACK
-        //        Legend l = chart.getLegend();
-        //        l.setEnabled(false);
+        val legend = chart.legend
+        legend.form = Legend.LegendForm.LINE
+        legend.typeface = Typeface.DEFAULT
+        legend.textColor = Color.BLACK
 
-        val xl = chart.xAxis
-        xl.typeface = Typeface.DEFAULT
-        xl.textColor = Color.BLACK
-        xl.setDrawGridLines(false)
-        xl.setAvoidFirstLastClipping(true)
-        xl.isEnabled = true
-        xl.setCenterAxisLabels(true)
-        xl.granularity = 1f // one hour
-        xl.position = XAxis.XAxisPosition.BOTTOM
+        val xAxis = chart.xAxis
+        xAxis.typeface = Typeface.DEFAULT
+        xAxis.textColor = Color.BLACK
+        xAxis.setDrawGridLines(false)
+        xAxis.setAvoidFirstLastClipping(true)
+        xAxis.isEnabled = true
+        xAxis.setCenterAxisLabels(true)
+        xAxis.granularity = 1f // one hour
+        xAxis.position = XAxis.XAxisPosition.BOTTOM
 
         val xAxisFormatter = HourAxisValueFormatter(mReferenceTimestamp)
-        xl.valueFormatter = xAxisFormatter
+        xAxis.valueFormatter = xAxisFormatter
 
         val leftAxis = chart.axisLeft
         leftAxis.typeface = Typeface.DEFAULT
         leftAxis.textColor = Color.BLACK
         leftAxis.axisMaximum = MAX_Y_AXIS
         leftAxis.axisMinimum = MIN_Y_AXIS
-        //        leftAxis.setSpaceTop(80);
-        //        leftAxis.setSpaceBottom(20);
 
         leftAxis.setDrawGridLines(true)
 
         val rightAxis = chart.axisRight
         rightAxis.isEnabled = false
-
     }
 
 
-    // Add new points to graph
-    private fun addGraphEntry(dataDisplay: IntArray) {
+    private fun addValuesToHistory(inputData: IntArray) {
         val data = chart.data
 
         if (data != null) {
             if (data.dataSetCount != 3)
-                for (temp in mDataList) {
+                for (temp in mDataList)
                     data.addDataSet(temp)
-                }
 
-            val now = System.currentTimeMillis() / 1000
-            data.addEntry(Entry((now - mReferenceTimestamp).toFloat(), dataDisplay[0].toFloat()), 0)
-            data.addEntry(Entry((now - mReferenceTimestamp).toFloat(), dataDisplay[1].toFloat()), 1)
-            data.addEntry(Entry((now - mReferenceTimestamp).toFloat(), dataDisplay[2].toFloat()), 2)
-
+            val now = now()
+            for (i in inputData.indices)
+                data.addEntry(Entry((now - mReferenceTimestamp).toFloat(), inputData[i].toFloat()), i)
 
             data.dataSets.forEach {
-                if (it.entryCount >= MAX_VISIBLE_ENTRIES) {
-                    info("TEST")
-                    for (i in 0 until MAX_REMOVED_ENTRIES) {
+                if (it.entryCount >= MAX_VISIBLE_ENTRIES)
+                    for (i in 0 until MAX_REMOVED_ENTRIES)
                         it.removeEntry(i)
-                    }
-
-                }
             }
 
             data.notifyDataChanged()
@@ -247,25 +193,17 @@ class IOIOFragment : RootFragment(), OnChartValueSelectedListener, FragmentLifec
             chart.notifyDataSetChanged()
             // invalidate data in case of data removal du to max entryCount value
             chart.invalidate()
-
         }
 
-        // limit the number of visible entries
         chart.setVisibleXRangeMaximum(MAX_X_RANGE)
-        // Sets the size of the area (range on the y-axis) that should be maximum visible at once
         chart.setVisibleYRangeMaximum(MAX_Y_RANGE, YAxis.AxisDependency.LEFT)
-        // chart.setVisibleYRange(30, AxisDependency.LEFT);
-        // move to the latest entry
-
-        //chart.moveViewToX(data.entryCount.toFloat())
-        // this automatically refreshes the chart (calls invalidate())
 
         val count = chart.data.getDataSetByIndex(1).entryCount
         val entry = chart.data.getDataSetByIndex(1).getEntryForIndex(count - 1)
-
-        //chart.moveViewToAnimated(entry.x, entry.y, YAxis.AxisDependency.LEFT,500)
         chart.moveViewTo(entry.x, entry.y, YAxis.AxisDependency.LEFT)
     }
+
+    private fun now() = System.currentTimeMillis() / 1000
 
 
     override fun onAttach(context: Context?) {
@@ -293,10 +231,8 @@ class IOIOFragment : RootFragment(), OnChartValueSelectedListener, FragmentLifec
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
-
                     if (it.isNotEmpty()) {
                         val device = it.first()
-
                         val gsonBuilder = GsonBuilder().registerTypeAdapter(IOIOData::class.java, DataDeserializer()).create()
                         val data = gsonBuilder.fromJson(device.data, IOIOData::class.java)
 
@@ -308,10 +244,7 @@ class IOIOFragment : RootFragment(), OnChartValueSelectedListener, FragmentLifec
                         fragment_ioio_progress_pm2.setValueAnimated(pm25.toFloat())
                         fragment_ioio_progress_pm10.setValueAnimated(pm10.toFloat())
 
-
-                        val dataToDisplay = intArrayOf(pm01, pm25, pm10)
-
-                        addGraphEntry(dataToDisplay)
+                        addValuesToHistory(intArrayOf(pm01, pm25, pm10))
                     } else {
                         fragment_ioio_progress_pm1.setValueAnimated(0.0f)
                         fragment_ioio_progress_pm2.setValueAnimated(0.0f)
@@ -320,7 +253,6 @@ class IOIOFragment : RootFragment(), OnChartValueSelectedListener, FragmentLifec
                 })
         info("onStart")
     }
-
 
     override fun onResume() {
         super.onResume()
@@ -340,9 +272,9 @@ class IOIOFragment : RootFragment(), OnChartValueSelectedListener, FragmentLifec
     }
 
     override fun onDestroyView() {
-        if (!mDisposable.isDisposed) {
+        if (!mDisposable.isDisposed)
             mDisposable.dispose()
-        }
+
         MoveViewJob.getInstance(null, 0f, 0f, null, null)
         super.onDestroyView()
         info("onDestroyView")
@@ -350,14 +282,13 @@ class IOIOFragment : RootFragment(), OnChartValueSelectedListener, FragmentLifec
 
     @SuppressLint("MissingSuperCall")
     override fun onDestroy() {
-        if (!mDisposable.isDisposed) {
+        if (!mDisposable.isDisposed)
             mDisposable.dispose()
-        }
+
         MoveViewJob.getInstance(null, 0f, 0f, null, null)
         super.onDestroy()
         info("onDestroy")
     }
-
 
     override fun onPauseFragment() {
         MoveViewJob.getInstance(null, 0f, 0f, null, null)
@@ -370,16 +301,13 @@ class IOIOFragment : RootFragment(), OnChartValueSelectedListener, FragmentLifec
 
     companion object {
         //TODO : allow user to change that value
-        // Graph params
         private const val MAX_VISIBLE_ENTRIES: Int = 100
         private const val MAX_REMOVED_ENTRIES: Int = 50
         private const val MAX_X_RANGE: Float = 40.0f
         private const val MAX_Y_RANGE: Float = 50.0f
         private const val MAX_Y_AXIS: Float = 3000.0f
         private const val MIN_Y_AXIS: Float = 0.0f
-        private const val MIN_TIME_STAMP: Long = 0
+        private const val MIN_TIMESTAMP: Long = 0
         private const val MAX_DEVICE: Long = 10
     }
 }
-
-
