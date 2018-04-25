46d935b640672ab87de352363f2b37dd72f4d373
Aoudia <mf.aoudia@gmail.com>
2018-04-26 00:55:40 +0200

    feat: Add Fastlane (Alpha)

--- scripts/writeEnvVariables.sh
+++ scripts/writeEnvVariables.sh
@@ -2,7 +2,6 @@
 
 function copyEnvVarsToManifest {
     ANDROID_MANIFEST=${TRAVIS_BUILD_DIR}"/app/src/main/AndroidManifest.xml"
-
     export ANDROID_MANIFEST
     echo "AndroidManifest should exist at $ANDROID_MANIFEST"
     sed -i -e "s/\"\${google_map_api_key}\"/"\"$GOOGLE_MAP_API_KEY\""/g" $ANDROID_MANIFEST
@@ -11,11 +10,17 @@ function copyEnvVarsToManifest {
 
 function copyEnvVarsToSigningProperties {
     KEYSTORE_AUTH=${TRAVIS_BUILD_DIR}"/keystore.properties"
-
     export KEYSTORE_AUTH
     echo "keystore.properties should exist at $KEYSTORE_AUTH"
     sed -i -e "s/myStorePassword/"$STORE_PASSWORD"/g" $KEYSTORE_AUTH
-    sed -i -e "s/mykeyPassword/"$KEY_PASSWORD"/g" $KEYSTORE_AUTH
+    sed -i -e "s/myKeyPassword/"$KEY_PASSWORD"/g" $KEYSTORE_AUTH
     sed -i -e "s/myKeyAlias/"$KEY_ALIAS"/g" $KEYSTORE_AUTH
     sed -i -e "s_myStoreFile_"${TRAVIS_BUILD_DIR}/${STORE_FILE}"_g" $KEYSTORE_AUTH
+}
+
+function copyEnvVarsToFastlaneConfiguration {
+    FASTLANE_CREDENTIAL=${TRAVIS_BUILD_DIR}"/fastlane/Appfile"
+    export FASTLANE_CREDENTIAL
+    echo "Appfile should exist at $FASTLANE_CREDENTIAL"
+    sed -i -e "s/\"\${secret_credentials}\"/"\"$FASTLANE_FILE\""/g" $FASTLANE_CREDENTIAL
 }
