ef2e0921821a94131460531718195bcbee5d9820
Lucas Declercq <lucas.declercq@ineat-conseil.fr>
2019-12-11 15:05:26 +0100

    fix(build): programmatically start an influxdb container instead of using an external one

--- app/src/test/java/science/apolline/InfluxDBServiceTest.kt
+++ app/src/test/java/science/apolline/InfluxDBServiceTest.kt
@@ -5,21 +5,31 @@ import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import org.junit.Assert
 import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import science.apolline.service.networks.*
-import science.apolline.models.Position
+import org.testcontainers.containers.wait.strategy.Wait
 import science.apolline.models.Device
+import science.apolline.models.Position
+import science.apolline.service.networks.ApiUtils
+import science.apolline.utils.GeoHashHelper
+import science.apolline.utils.InfluxDBContainer
 import science.apolline.utils.RequestParser
 import java.io.IOException
-import science.apolline.utils.GeoHashHelper
+
 
 /**
  * Created by sparow on 10/13/17.
  */
 
 class InfluxDBServiceTest {
+    companion object {
+        val INFLUXDB_DB = "test_db"
+        val INFLUXDB_USER = "test_user"
+        val INFLUXDB_USER_PASSWORD = "test_password"
+    }
 
-    private val testUrl = "http://localhost:8086/"
+    private var testUrl = "http://localhost:8086/"
     private var parser = JsonParser()
     private var JSONTOSEND = "{" +
             "\"CO2\":[100,\"PPM\"]," +
@@ -27,6 +37,24 @@ class InfluxDBServiceTest {
             "\"CH4\":[300,\"PPM\"]," +
             "\"O3\":[400,\"PPM\"]" +
             "}"
+
+    @Rule
+    @JvmField
+    val influxDBcontainer: InfluxDBContainer = InfluxDBContainer("influxdb:1.5.4")
+            .withEnv("INFLUXDB_DB",  INFLUXDB_DB)
+            .withEnv("INFLUXDB_USER", INFLUXDB_USER)
+            .withEnv("INFLUXDB_USER_PASSWORD", INFLUXDB_USER_PASSWORD)
+            .withExposedPorts(8086)
+            .waitingFor(Wait.forHttp("/ping").forStatusCode(204))
+
+    @Before
+    fun setUp() {
+        val containerIpAddress = influxDBcontainer.containerIpAddress
+        val firstMappedPort = influxDBcontainer.firstMappedPort
+
+        testUrl = String.format("http://%s:%s", containerIpAddress, firstMappedPort)
+    }
+
 
     /**
      * Test for input JSON parsing.
@@ -58,16 +86,16 @@ class InfluxDBServiceTest {
 
         val gson = Gson()
         val dataListObject = gson.fromJson(dataList, JsonObject::class.java)
-        val geohash = GeoHashHelper.encode(80.36,142.36)
+        val geohash = GeoHashHelper.encode(80.36, 142.36)
         val positionInitObject = Position("GPS", geohash, "Train")
-        val sensorInitObject = Device("ffffffff-c9cf-31db-0000-00006c125b14","Arduino", 1422568543702900257, positionInitObject, dataListObject,0)
+        val sensorInitObject = Device("ffffffff-c9cf-31db-0000-00006c125b14", "Arduino", 1422568543702900257, positionInitObject, dataListObject, 0)
 
         //when
         val dataTosend = RequestParser.createSingleRequestBody(sensorInitObject)
         println(dataTosend)
         ApiUtils.setUrl(testUrl)
         val api = ApiUtils.apiService
-        val call = api.savePost("test", "toto", "root", dataTosend)
+        val call = api.savePost(INFLUXDB_DB, INFLUXDB_USER, INFLUXDB_USER_PASSWORD, dataTosend)
         val response = call.execute()
 
         //then
@@ -78,9 +106,6 @@ class InfluxDBServiceTest {
     }
 
 }
-
-
-
 
 
 
